a1b4f4d3a8ccf0634e28f03a06ed85049d248362
diegocroquer-arch <diego.croquer@gm2dev.com>
2025-10-01 11:38:49 -0300

    fix: estructure api imagen

--- scripts/product-detail.js
+++ scripts/product-detail.js
@@ -1,17 +1,17 @@
 const URL = "https://fakestoreapi.com/products";
+let item = 1;
 
 fetch("https://fakestoreapi.com/products/1")
   .then((res) => res.json())
   .then((product) => {
-    console.log(product); // Aquí ves todos los datos
-    console.log("Imagen:", product.image); // URL de la imagen
-
-    // Crear un <img> y agregarlo al body
     const img = document.createElement("img");
     img.src = product.image;
     img.alt = product.title;
     img.width = 200;
-    document.body.appendChild(img);
+
+    // Buscar el div con id="products"
+    const container = document.getElementById("products");
+    container.appendChild(img); // 👈 mete la imagen dentro del div
   });
 const buttons = document.querySelectorAll(".size-btn button");
 buttons.forEach((btn) => {
