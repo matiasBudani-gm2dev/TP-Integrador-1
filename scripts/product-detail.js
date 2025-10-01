c35d86d7ab21d4b76ffc153580ac2525c1505f4b
Alex <alex.gioiosa@gm2dev.com>
2025-10-01 11:28:57 -0300

    button functions added

--- scripts/product-detail.js
+++ scripts/product-detail.js
@@ -13,3 +13,11 @@ fetch("https://fakestoreapi.com/products/1")
     img.width = 200;
     document.body.appendChild(img);
   });
+const buttons = document.querySelectorAll(".size-btn button");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
+document.querySelector(".Mbtn").classList.add("active");
